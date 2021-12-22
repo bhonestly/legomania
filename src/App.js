09eb1c179dd096949135b85b6af46071622ecbbe
polina-g <ignatieva.polina@gmail.com>
2021-12-22 15:13:41 -0500

    wip: adds API fetch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import './App.css';
 import {Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Home from './pages/Home';
 import Nav from './components/Nav';
 import Footer from './components/Footer'
 
+const {REACT_APP_BASE_URL, RREACT_APP_API_KEY} = process.env;
+
 function App() {
+  const [data, setData] = useState(null);
+
+  const getData = async () => {
+    const response = await fetch(`${REACT_APP_BASE_URL}/sets/?key=${RREACT_APP_API_KEY}`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    console.log(data);
+    setData(data);
+  }
+
+ useEffect(() => {
+   getData();
+ }, [])
+
   return (
     <div className='App'>
       <Nav />
       <Routes>
-        <Route path = '/' element={<Home text={'hello, world'}/>} />
+        <Route path = '/' element={<Home data={data}/>} />
       </Routes>
       <Footer />
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ const Home = (props) => {
     return (
         <main>
             <h1>Home Page</h1>
-            <p>{props.text}</p>
+            <p>{props.data}</p>
         </main>
     );
 };
