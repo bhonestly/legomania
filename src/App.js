f282f1be589d6a8f2d740731fd182ce01669b77f
polina-g <ignatieva.polina@gmail.com>
2021-12-27 14:33:17 -0500

    deletes dead code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import './App.css';
 import {Route, Routes } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import Home from './pages/Home';
 import Nav from './components/Nav';
 import Footer from './components/Footer'
 
-const {REACT_APP_BASE_URL, REACT_APP_API_KEY} = process.env;
-
 function App() {
-  // const [data, setData] = useState(null);
-
-  // const getData = async () => {
-  //   const response = await fetch(`${REACT_APP_BASE_URL}/sets/?key=${REACT_APP_API_KEY}`);
-  //   const data = await response.json();
-  //   console.log(data);
-  //   setData(data);
-  // }
-
-//  useEffect(() => {
-//    getData();
-//  }, [])
-
   return (
     <div className='App'>
       <Nav />
