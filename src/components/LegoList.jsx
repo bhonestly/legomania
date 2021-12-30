47c7202c386c0375edef9a5235164ea5f0b3fe84
bhonestly <ernestbizzell@gmail.com>
2021-12-30 19:13:01 +0300

    rendering list test

--- src/components/LegoList.jsx
+++ src/components/LegoList.jsx
@@ -1,22 +1,12 @@
 import React from 'react'
-import { getAllLegos } from '../services/api'
-import { useEffect, useState } from 'react'
-import Search from './Search'
 
-export default function LegoList() {
-  const [legoList, setLegoList] = useState([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await getAllLegos()
-      setLegoList(result.sort((a, b) => a.name.localeCompare(b.name))) 
-    };
-    fetchData();
-  }, []);
-
+export default function LegoList({list}) {
+  console.log(list)
   return (
-    <div>
-      <Search legoList={legoList} setLegoList={setLegoList} />
-    </div>
+    <ol>
+      {list.length && list.map((item, idx) => (
+        item
+      ))}
+    </ol>
   )
 }
