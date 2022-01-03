65af9a29500ff5019d38c8b9c501588d98dbf53d
bhonestly <ernestbizzell@gmail.com>
2022-01-03 21:24:28 +0300

    build

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import './FooterStyles.scss'
 const Footer = () => {
     return(
         <footer className='footer'>
-            <p>Copyright &copy; {new Date().getFullYear()} Legomania. All Rights Reserved.  Team Undefined - <a href="https://www.linkedin.com/in/kzbentley/">Kathy</a>, <a href="https://www.linkedin.com/in/sarah-marston-270">Sarah</a>, <a href="https://www.linkedin.com/in/shermeenr">Shermeen</a>, <a href="https://www.linkedin.com/in/polina-g/">Polina</a>, and <a href="https://www.linkedin.com/in/ernestbizzell/">Ernest</a></p>
+            <p>Copyright &copy; {new Date().getFullYear()} Legomania. All Rights Reserved.  Team Undefined - <a href="https://www.linkedin.com/in/kzbentley/">Kathryn</a>, <a href="https://www.linkedin.com/in/sarah-marston-270">Sarah</a>, <a href="https://www.linkedin.com/in/shermeenr">Shermeen</a>, <a href="https://www.linkedin.com/in/polina-g/">Polina</a>, and <a href="https://www.linkedin.com/in/ernestbizzell/">Ernest</a></p>
         </footer>
     );
 };
